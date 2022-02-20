6c0ab0a4113d85c8d43e41377ad2fb76bcb6551a
oswald921 <oswaldy971@gmail.com>
2022-02-20 21:38:57 +0900

    フッターに名前を追加

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="footer">
     <div>
       This Application is made by
-      <span class="names">○○○, ○○○ and ○○○</span>.
+      <span class="names">五十嵐梨玖, 服部美咲 and 森田陽菜</span>.
     </div>
     <small
       >The Application is created as a part of the GeekSalon WebExpert
